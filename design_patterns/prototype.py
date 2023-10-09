201e42fc4d54bc668ae25aa72301134039089abb
ooknimm <ooknimm@gmail.com>
2023-10-09 23:10:28 +0900

    prototype

---
+++ design_patterns/prototype.py
@@ -0,0 +1,13 @@
+import abc
+
+class Shape(abc.ABC):
+    @abc.abstractmethod
+    def clone(self) -> "Shape": ...
+
+class Rectangle(Shape):
+    def __init__(self, width: int, height: int) -> None:
+        self.width = width
+        self.height = height
+
+    def clone(self) -> Shape:
+        return Rectangle(self.width, self.height)
